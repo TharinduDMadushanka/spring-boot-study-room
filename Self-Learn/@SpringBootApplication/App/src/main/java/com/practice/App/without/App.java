a6aebc3cf90165f3953bda0ef54be2dfc56f5180
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-28 19:32:31 +0530

    without @SpringBootApplication annotation

---
+++ Self-Learn/@SpringBootApplication/App/src/main/java/com/practice/App/without/App.java
@@ -0,0 +1,31 @@
+package com.practice.App.without;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("This is without @SpringBootApplication annotation");
+    }
+}
+
+
+/**
+ * @SpringBootApplication == @Configuration + @EnableAutoConfiguration + @ComponentScan
+
+ * @Configuration -> indicates that the class can be used by the Spring IoC container as a source of bean definitions
+
+ * @EnableAutoConfiguration -> tells Spring Boot to start adding beans based on classpath settings, other beans,
+   and various property settings
+
+ * @ComponentScan -> tells Spring to scan for components, configurations, and services in the specified package
+
+ ** By using @SpringBootApplication, you simplify the configuration of your Spring Boot application by combining these
+    three annotations into one
+ */
