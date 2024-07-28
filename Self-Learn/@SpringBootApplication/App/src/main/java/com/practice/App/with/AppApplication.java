10e8a3fc747c630d4f3d84eaee7cfc00e50dbae3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-28 19:25:29 +0530

    with @SpringBootApplication annotation

---
+++ Self-Learn/@SpringBootApplication/App/src/main/java/com/practice/App/with/AppApplication.java
@@ -0,0 +1,14 @@
+package com.practice.App.with;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+		System.out.println("This is with @SpringBootApplication annotation");
+	}
+
+}
