29a3c0cda8ce24a363d9fca3a2d4002b0dd6b46d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-29 15:09:48 +0530

    without @RestController  annotation

--- Self-Learn/Annotations/App/src/main/java/com/practice/App/RestController/with/RestController.java
+++ Self-Learn/Annotations/App/src/main/java/com/practice/App/RestController/with/RestController.java
@@ -12,3 +12,11 @@ public class RestController {
     }
 
 }
+
+/**
+ * @RestController ->
+        * used on the RestController class, which handles HTTP GET requests to the "/hello" endpoint.
+        * The hello method returns a plain string, which will be written directly to the HTTP response body as JSON.
+
+ * @GetMapping annotation -> is used for mapping HTTP GET requests onto specific handler methods
+ */
