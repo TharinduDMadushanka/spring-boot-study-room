c2fc6115de38edeb763cf2c1f65f93d19d0637de
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-30 10:58:08 +0530

    return string value in to web

---
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/HomeController.java
@@ -0,0 +1,22 @@
+package com.tdm.simpleWebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello Welcome";
+    }
+
+}
+
+/**
+ * @ Controller not return the string into web
+ * but we add @ResponseBody to the method it will shoe
+
+ * to achieve it easily we can simply add @RestController for the class instead that 2 annotations
+ */
