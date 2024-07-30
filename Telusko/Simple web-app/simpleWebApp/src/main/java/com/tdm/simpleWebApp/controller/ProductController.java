c07b5a4ea86300538f33b116b85819bbd4d2a43c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-30 12:12:15 +0530

    logic for getProductById using java stream api

--- Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/controller/ProductController.java
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.tdm.simpleWebApp.controller;
 import com.tdm.simpleWebApp.model.Products;
 import com.tdm.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,17 @@ public class ProductController {
         return service.getProducts();
     }
 
-    public Products getProductById(int prodId){
+    @RequestMapping("/product/{prodId}")
+    public Products getProductById(@PathVariable int prodId){
         return service.getProductById(prodId);
     }
 
 }
+
+/**
+ * {prodId} -> the value came in the request will be mapped in to prodId
+ * if we want to match these 2 we should use @PathVariable this annotation
+
+ *   @RequestMapping("/product/102") -> if we send request like this the error shows
+ "Optional int parameter 'prodId' is present but cannot be translated into a null value due to being declared as a primitive type. "
+ */
