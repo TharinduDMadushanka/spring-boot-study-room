38f696eae2a3a7e43b30684db6aa97219b00d879
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-30 11:27:16 +0530

    create endpoint called product and handle the request

--- Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/controller/ProductController.java
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/controller/ProductController.java
@@ -1,13 +1,22 @@
 package com.tdm.simpleWebApp.controller;
 
+import com.tdm.simpleWebApp.model.Products;
 import com.tdm.simpleWebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class ProductController {
 
+    @Autowired
     ProductService service;
 
-    public String getProducts(){
-        return "";
+    @RequestMapping("/product")
+    public List<Products> getProducts(){
+        return service.getProducts();
     }
 
 }
