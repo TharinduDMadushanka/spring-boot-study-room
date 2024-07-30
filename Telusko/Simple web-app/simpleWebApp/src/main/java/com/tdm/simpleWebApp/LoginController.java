d156d7e918f563759578db36b46d046519c15f74
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-30 11:03:42 +0530

    add LoginController and test if its works

---
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/LoginController.java
@@ -0,0 +1,20 @@
+package com.tdm.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "Logging page..!";
+    }
+
+}
+
+/**
+ * we can add any of controller class
+ * simply request going in to the front controller who makes us for spring framework
+ * and it looking for all of Request handle annotations
+ */
