bb7e95704cea341824f1d8b7fb341709f6a4475e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-31 09:57:06 +0530

    add @Entity & @Id for entity class(model)

--- Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/model/Products.java
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/model/Products.java
@@ -23,4 +23,6 @@ public class Products {
 /**
  * @Data -> this is from lombok dependency.It will do add getter setter and  behind the scene
  * @AllArgsConstructor -> constructors create
+ * @Entity -> used at the class level and marks the class as a persistent entity.
+ * @Id -> this member will uniquely identify the entity in the database.(Primary Key)
  */
