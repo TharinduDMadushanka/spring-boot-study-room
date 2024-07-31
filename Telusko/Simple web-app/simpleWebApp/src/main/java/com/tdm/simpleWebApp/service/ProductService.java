36e1ba354e68518400b0238fe1acaf30d89a786e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-31 09:32:35 +0530

    commented old implementation logics of requests

--- Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/service/ProductService.java
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/service/ProductService.java
@@ -15,44 +15,44 @@ public class ProductService {
     @Autowired
     ProductRepo repo; // instance of ProductRepo interface
 
-    List<Products> products = new ArrayList<>(Arrays.asList(
-            new Products(101, "Iphone", 50000),
-            new Products(102, "Samsung", 70000),
-            new Products(103, "Canon Camera", 100000)
-
-    ));
+//    List<Products> products = new ArrayList<>(Arrays.asList(
+//            new Products(101, "Iphone", 50000),
+//            new Products(102, "Samsung", 70000),
+//            new Products(103, "Canon Camera", 100000)
+//
+//    ));
 
     public List<Products> getProducts() {
-        return products;
+//        return products;
     }
 
     public Products getProductById(int prodId) {
-        return products.stream()
-                .filter(p -> p.getProdId() == prodId)
-                .findFirst().get();
+//        return products.stream()
+//                .filter(p -> p.getProdId() == prodId)
+//                .findFirst().get();
     }
 
     public void addProducts(Products prod) {
-        products.add(prod);
+//        products.add(prod);
     }
 
     public void updateProduct(Products prod) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prod.getProdId())
-                index = i;
-
-            products.set(index, prod);
-        }
+//        int index = 0;
+//        for (int i = 0; i < products.size(); i++) {
+//            if (products.get(i).getProdId() == prod.getProdId())
+//                index = i;
+//
+//            products.set(index, prod);
+//        }
     }
 
     public void deleteProduct(int prodId) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prodId)
-                index = i;
-
-            products.remove(index);
-        }
+//        int index = 0;
+//        for (int i = 0; i < products.size(); i++) {
+//            if (products.get(i).getProdId() == prodId)
+//                index = i;
+//
+//            products.remove(index);
+//        }
     }
 }
