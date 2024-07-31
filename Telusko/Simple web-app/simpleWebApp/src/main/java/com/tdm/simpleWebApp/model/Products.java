cc596bdbeefd129112571f5bf4435f7c5c34c008
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-31 09:46:35 +0530

    add @Entity & @Id for entity class(model)

--- Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/model/Products.java
+++ Telusko/Simple web-app/simpleWebApp/src/main/java/com/tdm/simpleWebApp/model/Products.java
@@ -1,15 +1,22 @@
 package com.tdm.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Products {
 
+    @Id
     private int prodId;
     private String prodName;
     private int price;
+
+    public Products() {
+    }
 
 }
 
