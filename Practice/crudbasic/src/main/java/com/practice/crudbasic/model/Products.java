f6d54bf39fa5e6f73c3ce546265eb61838aa51ed
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-01 07:45:10 +0530

    create Products.java in model

--- Practice/crudbasic/src/main/java/com/practice/crudbasic/model/Products.java
+++ Practice/crudbasic/src/main/java/com/practice/crudbasic/model/Products.java
@@ -1,2 +1,20 @@
-package com.practice.crudbasic.model;public class Products {
+package com.practice.crudbasic.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Products {
+
+    @Id
+    private int prodId;
+    private String prodName;
+    private int price;
+
 }
