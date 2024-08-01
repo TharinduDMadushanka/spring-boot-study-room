d4c9bc9b43a18d3d4b673ce4fbbb3a9f7525e57e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-01 07:50:46 +0530

    create ProductService.java in service

---
+++ Practice/crudbasic/src/main/java/com/practice/crudbasic/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.practice.crudbasic.service;
+
+import com.practice.crudbasic.model.Products;
+import com.practice.crudbasic.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo repo;
+
+    public List<Products> getProducts(){
+        return repo.findAll();
+    }
+
+    public Products getProductById(int id){
+        return repo.findById(id).get().orElse(null);
+    }
+
+    public void addProduct(Products prod){
+        repo.save(prod);
+    }
+
+    public void updateProduct(Products prod){
+        repo.save(prod);
+    }
+
+    public void deleteProduct(int id){
+        repo.deleteById(id);
+    }
+
+}
