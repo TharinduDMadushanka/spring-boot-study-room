0be7763b7d5962a74ee0eab25e62f449490c3ae6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-08 13:40:08 +0530

    explained how @PropertySource works

--- Self-Learn/Annotations/App/src/main/java/com/practice/App/PropertySource/AppConfig.java
+++ Self-Learn/Annotations/App/src/main/java/com/practice/App/PropertySource/AppConfig.java
@@ -19,3 +19,8 @@ public class AppConfig {
         System.out.println("App Version: " + appVersion);
     }
 }
+
+/**
+ * @PropertySource("classpath:application.properties.test") -> ells Spring to load properties from the app.properties file.
+ * @Value("${app.name}") and @Value("${app.version}") inject the properties into the fields.
+ */
