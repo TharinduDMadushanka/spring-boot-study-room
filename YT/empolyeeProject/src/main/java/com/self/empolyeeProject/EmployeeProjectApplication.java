ca10abeee38792cd0fc33fe5522b6f11fb428eaf
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-05 09:16:21 +0530

    created save method in service layer

--- YT/empolyeeProject/src/main/java/com/self/empolyeeProject/EmployeeProjectApplication.java
+++ YT/empolyeeProject/src/main/java/com/self/empolyeeProject/EmployeeProjectApplication.java
@@ -1,13 +1,20 @@
 package com.self.empolyeeProject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProjectApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
--- YT/empolyeeProject/src/main/java/com/self/empolyeeProject/service/EmployeeService.java
+++ YT/empolyeeProject/src/main/java/com/self/empolyeeProject/service/EmployeeService.java
@@ -1,4 +1,33 @@
 package com.self.empolyeeProject.service;
 
+import com.self.empolyeeProject.dto.EmployeeDTO;
+import com.self.empolyeeProject.entity.Employee;
+import com.self.empolyeeProject.repo.EmployeeRepo;
+import com.self.empolyeeProject.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public String saveEmployee(EmployeeDTO employeeDTO) {
+
+        if (employeeRepo.existsById(employeeDTO.getEmpID())) {
+            return VarList.RSP_DUPLICATED;
+        } else {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+        }
+    }
+
 }
