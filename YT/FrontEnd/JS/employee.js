09d1257c08752540e4ec6902689760a81aa2051a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-05 12:02:56 +0530

    completed save/delete/update/getall functions in frontend

--- YT/FrontEnd/JS/employee.js
+++ YT/FrontEnd/JS/employee.js
@@ -1,3 +1,5 @@
+getAllEmployees();
+
 function saveEmployee(){
 
     let name = $('#name').val();
@@ -18,9 +20,89 @@ function saveEmployee(){
         }),
         success: function (data){
             alert("employee saved.!")
+            getAllEmployees();
         },
         error: function (xhr, exception){
             alert("error at saved.!")
+        }
+    })
+}
+
+function updateEmployee(){
+
+    let empID= $('#empID').val()
+    let name = $('#name').val();
+    let address = $('#address').val();
+    let number = $('#mobile').val();
+
+    // console.log(name);
+    $.ajax({
+        method:"PUT",
+        contentType: "application/json",
+        url:"http://localhost:8080/api/v1/emp/updateEmp",
+        async:true,
+        data: JSON.stringify({
+            "empID":empID,
+            "empName": name,
+            "empAddress" : address,
+            "empMNumber" : number
+        }),
+        success: function (data){
+            alert("employee updated.!")
+            getAllEmployees();
+        },
+        error: function (xhr, exception){
+            alert("error at update.!")
+        }
+    })
+}
+
+function deleteEmployee(){
+
+    let empID= $('#empID').val()
+
+    $.ajax({
+        method:"DELETE",
+        url:"http://localhost:8080/api/v1/emp/deleteEmp/"+empID,
+        async:true,
+        success: function (data){
+            alert("employee deleted.!")
+            getAllEmployees();
+        },
+        error: function (xhr, exception){
+            alert("error at delete.!")
+        }
+    })
+}
+
+function getAllEmployees(){
+
+    $.ajax({
+        method:"GET",
+        url:"http://localhost:8080/api/v1/emp/getAllEmp",
+        async:true,
+        success: function (data){
+
+            if (data.code==="00"){
+                $('#empTable').empty();
+                for (let emp of data.content){
+                    let empID = emp.empID
+                    let name = emp.empName
+                    let address = emp.empAddress
+                    let number = emp.empMNumber
+
+                    var row = `<tr>
+                                        <td>${empID}</td>
+                                        <td>${name}</td>
+                                        <td>${address}</td>
+                                        <td>${number}</td>
+                                      </tr>`;
+                    $('#empTable').append(row)
+                }
+            }
+        },
+        error: function (xhr, exception){
+            alert("error at load.!")
         }
     })
 }
