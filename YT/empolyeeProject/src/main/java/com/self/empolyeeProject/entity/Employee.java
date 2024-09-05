1b82cdcf4be62ffec4638cd6f5c2f84f463a8b23
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-05 09:00:55 +0530

    create employee project employee Entity

---
+++ YT/empolyeeProject/src/main/java/com/self/empolyeeProject/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.self.empolyeeProject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empID;
+    private String empName;
+    private String empAddress;
+    private String empMNumber;
+
+}
---
+++ YT/empolyeeProject/src/test/java/com/self/empolyeeProject/EmpolyeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.empolyeeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpolyeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
