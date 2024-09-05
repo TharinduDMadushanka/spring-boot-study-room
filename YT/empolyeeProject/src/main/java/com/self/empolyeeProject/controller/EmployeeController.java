943ce7929450f9a6ed58bc7d95a2ec879973219a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-05 10:43:49 +0530

    completed update employee

--- YT/empolyeeProject/src/main/java/com/self/empolyeeProject/controller/EmployeeController.java
+++ YT/empolyeeProject/src/main/java/com/self/empolyeeProject/controller/EmployeeController.java
@@ -34,10 +34,53 @@ public class EmployeeController {
 
                 return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
 
-            }else if (resp.equals("06")){
+            } else if (resp.equals("06")) {
 
                 responseDTO.setCode(VarList.RSP_DUPLICATED);
                 responseDTO.setMessage("Employee Registered");
+                responseDTO.setContent(employeeDTO);
+
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            } else {
+
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            }
+
+        } catch (Exception e) {
+
+            responseDTO.setCode(VarList.RSP_FAIL);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+    @PutMapping("/updateEmp")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+
+        try {
+
+            String resp = employeeService.updateEmployee(employeeDTO);
+            if (resp.equals("00")) {
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+            }else if (resp.equals("01")){
+
+                responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("Not a registered employee");
                 responseDTO.setContent(employeeDTO);
 
                 return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
