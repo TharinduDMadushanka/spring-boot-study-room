7de0a978ecf270fe9d570a78edfdd3fc909889ec
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-06 07:54:30 +0530

    creat and customize a project for practice fullstack with login form project

---
+++ Practice/LoginForm/src/test/java/com/tdm/LoginForm/LoginFormProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdm.LoginForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFormProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
