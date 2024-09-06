779c4bd835b5dd827cb5625429c4de81cfd1f75e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-06 11:34:37 +0530

    add backToLogin button in to the dashboard

--- Practice/LoginForm/src/frontend/src/pages/Dashboard.tsx
+++ Practice/LoginForm/src/frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
 // @ts-ignore
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Dashboard(){
+
+    const navigate=useNavigate();
+
+    async function backToLogin(){
+        navigate('/login');
+    }
 
     return (
         <div className="container" style={{
@@ -12,8 +19,14 @@ function Dashboard(){
             margin: 'auto',
             marginTop: '100px'
         }}>
-            <h1 style={{ color: '#fff' }}>Welcome to the Dashboard</h1>
-            {/* Add additional dashboard content here */}
+            <h1 style={{color: '#fff'}}>Welcome to the Dashboard</h1>
+            <button className="btn btn-danger" onClick={backToLogin}  style={{
+                width: '100px',
+                marginTop: '10px',  // Optional: Adjust space between title and button
+                position: 'absolute',
+                top: '150px',
+                right: '200px',
+            }}>Logout</button>
         </div>
     );
 
