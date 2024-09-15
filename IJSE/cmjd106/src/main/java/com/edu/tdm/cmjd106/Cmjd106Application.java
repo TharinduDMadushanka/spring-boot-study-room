3e1c0eb1fdb6c94ced8be3fd7b535f01c3ac7662
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-15 07:16:22 +0530

    project for study IJSE-CMJD project understand

---
+++ IJSE/cmjd106/src/main/java/com/edu/tdm/cmjd106/Cmjd106Application.java
@@ -0,0 +1,13 @@
+package com.edu.tdm.cmjd106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmjd106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmjd106Application.class, args);
+	}
+
+}
---
+++ IJSE/cmjd106/src/test/java/com/edu/tdm/cmjd106/Cmjd106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.tdm.cmjd106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmjd106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
