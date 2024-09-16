9a75b5df8bfbc2b1b656f9f82dcd592a4bd6152a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-16 08:54:04 +0530

    created loadProduct function in Product page

--- IJSE/cmjd106/src/Frontend/src/pages/Product.tsx
+++ IJSE/cmjd106/src/Frontend/src/pages/Product.tsx
@@ -1,7 +1,32 @@
+import {useEffect, useState} from "react";
+import ProductType from "../types/ProductType.tsx";
+import axios from "axios";
+
 function Product(){
+
+    const [product, setProduct] = useState<ProductType[]>([]);
+
+    async function loadProduct(){
+        const response = await axios.get("http://localhost:8080/products")
+        setProduct(response.data);
+    }
+
+    useEffect(function (){
+        loadProduct();
+    },[])
+
     return(
         <div>
             <h1>Product</h1>
+
+            {product.map(function (product){
+                return(
+                    <div>
+                        <h2>{product.name}</h2>
+                    </div>
+                );
+            })}
+
         </div>
     );
 }
